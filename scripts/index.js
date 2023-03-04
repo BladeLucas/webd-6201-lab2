1126088f1991e1310454796a8caba110d3dc0ed1
Blade Lucas <elffe2@hotmail.ca>
2023-03-04 16:06:43 -0500

    Added jquery logic

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,76 @@ class User {
         this.lastName = lastName
         this.username = username
         this.email = email
-        this. password = password
+        this.password = password
+    }
+}
+
+function displayNameInNav() {
+    var username = $("#usernameInput").val()
+    var password = $("#passwordInput").val()
+
+    if (username != "" && password != "") {
+        if ($("#usernameDisplay").length == 0) {
+            $("#navbarNavAltMarkup").after("<span class='navbar-text me-5' id='usernameDisplay'>" + username + "</span>")
+        }
+    }
+ }
+
+function errorMessageDiv() {
+
+    var error = false
+
+    if ($("#ErrorMessage").length == 0) {
+        $("#registerForm").after("<div id='ErrorMessage'></div>")
+        $("#ErrorMessage").hide()
+    } else {
+        $("#ErrorMessage").html("")
+    }
+
+    var firstName = $("#firstNameInput").val()
+    var lastName = $("#lastNameInput").val()
+    var email = $("#emailInput").val()
+    var password = $("#passwordInput").val()
+    var confirmPassword = $("#confirmPasswordInput").val()
+
+    if (firstName.length < 2 || lastName.length < 2) {
+        $("#ErrorMessage").show()
+        $("#ErrorMessage").html("<p>The First and Last Name needs to be 2 characters or more.</p>")
+        error = true
+    }
+
+    if (email.length < 8 || email.indexOf("@") == -1) {
+        $("#ErrorMessage").show()
+        $("#ErrorMessage").html("<p>The email address must be at least 8 characters long and include the @ symbol.</p>")
+        error = true
+    }
+
+    if (password.length < 6 || password !== confirmPassword) {
+        $("#ErrorMessage").show()
+        $("#ErrorMessage").html("<p>The password must match the confirm password field and be at least 6 characters long.</p>")
+        error = true
+    }
+
+    if (!error) {
+        var user = new User(firstName, lastName, email, email, password)
+        console.log(user)
+
+        $("#registerForm")[0].reset()
     }
 }
 
 (function () {
-    function displayNameInNav() {
-        
+    addEventListener('submit', submitForm => {
+        event.preventDefault()
+    })
+
+    function displayUserInConsole() {
+
     }
+
+    function Start() {
+        console.log("App Started!")
+    }
+
+    window.addEventListener("load", Start)
 })()
